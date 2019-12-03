c86c5636f593d351d95d5eb47dbcce0193dd6ec0
mads6527 <mads6527@hotmail.com>
2019-12-03 16:22:16 +0100

    Repositories

---
+++ DABAssignment3/DABAssignment3/Repository/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DABAssignment3.Repository
+{
+    interface IRepository<Tentity> where Tentity : class
+    {
+        Task<Tentity> GetAsync(int id);
+        Task<IEnumerable<Tentity>> GetAllAsync();
+        IEnumerable<Tentity> Find(Expression<Func<TEntity, bool>> predicate);
+
+        Task AddAsync(Tentity entity);
+        Task AddRangeAsync(IEnumerable<Tentity> entities);
+
+        void Remove(int id);
+        void RemoveRange(IEnumerable<Tentity> entities);
+
+        void Update(Tentity entity);
+
+        Task SaveChangesasync();
+        void SaveChanges();
+    }
+}
