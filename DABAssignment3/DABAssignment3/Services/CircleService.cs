4d519bcf0d7082fec01a10d2a340ad4ba225756a
maxxum12@gmail.com <maxxum12@gmail.com>
2019-12-05 10:57:47 +0100

    Services update

--- DABAssignment3/DABAssignment3/Services/CircleService.cs
+++ DABAssignment3/DABAssignment3/Services/CircleService.cs
@@ -3,41 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DABAssignment3.Models;
+using MongoDB.Driver;
 
 namespace DABAssignment3.Services
 {
     public class CircleService : ICircleService
     {
-
-
-        public List<Circle> GetAll()
+        private readonly IMongoCollection<Circle> _circles;
+        public CircleService()
         {
-            throw new NotImplementedException();
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _circles = database.GetCollection<Circle>(settings.BooksCollectionName);
+
         }
 
-        public Circle Get(string Id)
-        {
-            throw new NotImplementedException();
-        }
+        public List<Circle> GetAll() =>
+            _circles.Find(book => true).ToList();
+
+        public Circle Get(string Id) =>
+            _circles.Find<Circle>(circle => circle.CircleId == Id).FirstOrDefault();
 
         public Circle Create(Circle circle)
         {
-            throw new NotImplementedException();
+            _circles.InsertOne(circle);
+            return circle;
         }
 
-        public void Update(string id, Circle circle)
-        {
-            throw new NotImplementedException();
-        }
+        public void Update(string id, Circle circle) =>
+            _circles.ReplaceOne(circle => circle.CircleId == id, circle);
 
-        public void Remove(Circle circle)
-        {
-            throw new NotImplementedException();
-        }
+        public void Remove(Circle circleIn) =>
+            _circles.DeleteOne(circle => circle.CircleId == circleIn.CircleId);
 
-        public void Remove(string id)
-        {
-            throw new NotImplementedException();
-        }
+        public void Remove(string id) =>
+            _circles.DeleteOne(circle => circle.CircleId == id);
     }
 }
--- DABAssignment3/DABAssignment3/Services/PostService.cs
+++ DABAssignment3/DABAssignment3/Services/PostService.cs
@@ -3,39 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DABAssignment3.Models;
+using MongoDB.Driver;
 
 namespace DABAssignment3.Services
 {
     public class PostService : IPostService
     {
-        public List<Post> GetAll()
+        private readonly IMongoCollection<Post> _posts;
+
+        public PostService()
         {
-            throw new NotImplementedException();
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _posts = database.GetCollection<Post>(settings.BooksCollectionName);
+
         }
 
-        public Post Get(string Id)
-        {
-            throw new NotImplementedException();
-        }
+        public List<Post> GetAll() =>
+            _posts.Find(post => true).ToList();
+
+        public Post Get(string Id) =>
+            _posts.Find<Post>(post => post.PostId == Id).FirstOrDefault();
 
         public Post Create(Post Post)
         {
-            throw new NotImplementedException();
+            _posts.InsertOne(Post);
+            return Post;
         }
 
-        public void Update(string id, Post Post)
-        {
-            throw new NotImplementedException();
-        }
+        public void Update(string id, Post Post) =>
+            _posts.ReplaceOne(post => post.PostId.ToString == id, Post);
 
-        public void Remove(Post Post)
-        {
-            throw new NotImplementedException();
-        }
+        public void Remove(Post Post) =>
+            _posts.DeleteOne(post => post.PostId == Post.PostId);
 
-        public void Remove(string id)
-        {
-            throw new NotImplementedException();
-        }
+        public void Remove(string id) =>
+            _posts.DeleteOne(post => post.PostId == id);
     }
 }
