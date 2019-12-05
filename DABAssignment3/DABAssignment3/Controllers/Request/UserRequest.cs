c8e851d7f409f06bccfa1de47fce686504e9e0a1
maxxum12@gmail.com <maxxum12@gmail.com>
2019-12-05 11:32:44 +0100

    user

---
+++ DABAssignment3/DABAssignment3/Controllers/Request/UserRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DABAssignment3.Controllers.Request
+{
+    public class UserRequest
+    {
+        public string name;
+    }
+}
---
+++ DABAssignment3/DABAssignment3/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DABAssignment3.Controllers.Request;
+using DABAssignment3.Controllers.Response;
+using DABAssignment3.Models;
+using DABAssignment3.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DABAssignment3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userservice;
+
+        public UserController(UserService userService)
+        {
+            _userservice = userService;
+        }
+        // GET: api/User
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public IActionResult<UserResponse> Post([FromBody] UserRequest request)
+        {
+            _userservice.Create(request);
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
