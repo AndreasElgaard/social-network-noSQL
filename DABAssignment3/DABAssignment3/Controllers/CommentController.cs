ae5e620cca360ecac86c1d3ab4adb6715eb6060f
mads6527 <mads6527@hotmail.com>
2019-12-05 11:33:15 +0100

    Mapper

---
+++ DABAssignment3/DABAssignment3/Controllers/CommentController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using DABAssignment3.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DABAssignment3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentService _commentService;
+        private readonly IMapper _mapper;
+
+        public CommentController(ICommentService commentService, IMapper mapper)
+        {
+            _commentService = commentService;
+            _mapper = mapper;
+        }
+        // GET: api/Comment
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Comment/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Comment
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Comment/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DABAssignment3/DABAssignment3/Mapping/DomainToDtoProfile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using DABAssignment3.Models;
+
+namespace DABAssignment3.Mapping
+{
+    public class DomainToDtoProfile : Profile
+    {
+        public DomainToDtoProfile()
+        {
+            CreateMap<Circle, CircleResponse>();
+
+            CreateMap<Post, PostResponse>();
+
+            CreateMap<User, UserResponse>();
+
+            CreateMap<Comment, CommentResponse>();
+
+            CreateMap<CircleRequest, Circle>();
+
+            CreateMap<PostRequest, Post>();
+
+            CreateMap<UserRequest, User>();
+
+            CreateMap<CommentRequest, Comment>();
+        }
+    }
+}
