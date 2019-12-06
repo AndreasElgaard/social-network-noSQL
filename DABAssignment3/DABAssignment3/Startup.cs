156ae448225b95650d966c1e09c078348887b81d
maxxum12@gmail.com <maxxum12@gmail.com>
2019-12-06 15:13:32 +0100

    blabla

--- DABAssignment3/DABAssignment3/Startup.cs
+++ DABAssignment3/DABAssignment3/Startup.cs
@@ -19,6 +19,7 @@ namespace DABAssignment3
 {
     public class Startup
     {
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -43,6 +44,8 @@ namespace DABAssignment3
             services.AddControllers();
 
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "DabApi", Version = "v1"}); });
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -75,5 +78,7 @@ namespace DABAssignment3
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
+
+        public void
     }
 }
